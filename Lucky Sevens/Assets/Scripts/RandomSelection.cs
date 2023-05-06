ba5d3747cf27c12fb067580a9ab0313657fb1a8e
Lusenio (Josh) <joshua.hadlock+Github@gmail.com>
2023-05-06 03:29:23 -0400

    Random Door Selector Update 2
    You can now trigger the selection with the Enter key. It will roll for a few seconds and then stop and make the winning open blink green.

--- Lucky Sevens/Assets/Scripts/RandomSelection.cs
+++ Lucky Sevens/Assets/Scripts/RandomSelection.cs
@@ -16,6 +16,11 @@ public class RandomSelection : MonoBehaviour
     int result;
     int highlight;
     float currShiftTime;
+
+    [Header("--- Results Stats ---")]
+    [Range(0.1f, 10)][SerializeField] float blinkTime;
+
+    bool blinking;
     [Header("--- Rigged Settings ---")]
     [SerializeField] bool rigged;
     [SerializeField] int riggedNum;
@@ -23,19 +28,16 @@ public class RandomSelection : MonoBehaviour
     void Start()
     {
         result = -1;
+        rolling = false;
+        blinking = false;
+
     }
     void Update()
     {
         currShiftTime -= Time.deltaTime;
         if(currShiftTime <= 0 && result == -1)
         {
-            _lightObjects[highlight].GetComponent<Renderer>().material.color = Color.black;
-            highlight++;
-            if(highlight >= _lightObjects.Length)
-            {
-                highlight = 0;
-            }
-            _lightObjects[highlight].GetComponent<Renderer>().material.color = Color.yellow;
+            Shift();
             if (rolling != true)
             {
                 currShiftTime = idleShiftTime;
@@ -44,6 +46,55 @@ public class RandomSelection : MonoBehaviour
             {
                 currShiftTime = activeShiftTime;
             }
+        }
+        else if (!blinking && result != -1)
+        {
+            StartCoroutine(Blink());
+        }
+        if (Input.GetButtonDown("Submit") && result == -1)
+        {
+            Debug.Log("Hoi am Tem");
+            StartCoroutine(Roll());
+        }
+    }
+
+    void Shift()
+    {
+        _lightObjects[highlight].GetComponent<Renderer>().material.color = Color.black;
+        highlight++;
+        if (highlight >= _lightObjects.Length)
+        {
+            highlight = 0;
+        }
+        _lightObjects[highlight].GetComponent<Renderer>().material.color = Color.yellow;
+    }
+
+    IEnumerator Blink()
+    {
+        blinking = true;
+        yield return new WaitForSeconds(blinkTime);
+        blinking = false;
+        if(_lightObjects[highlight].GetComponent<Renderer>().material.color == Color.green)
+        {
+            _lightObjects[highlight].GetComponent<Renderer>().material.color = Color.black;
+        }
+        else
+        {
+            _lightObjects[highlight].GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+    IEnumerator Roll()
+    {
+        rolling = true;
+        yield return new WaitForSeconds(rollTime);
+        if(rigged && riggedNum !>= _randomObjects.Length)
+        {
+            result = riggedNum;
+        }
+        else
+        {
+            result = highlight;
+            _lightObjects[highlight].GetComponent<Renderer>().material.color = Color.green;
         }
     }
 }
