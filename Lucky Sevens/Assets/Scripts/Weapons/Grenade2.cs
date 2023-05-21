51dcb98e7152d078e6bfc89ac77e03244ff3b0ec
Najee Jackson <NJackson1@student.fullsail.edu>
2023-05-21 16:49:03 -0400

    Fixed grenade 2 script should work fine now

--- Lucky Sevens/Assets/Scripts/Weapons/Grenade2.cs
+++ Lucky Sevens/Assets/Scripts/Weapons/Grenade2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grenade2 : MonoBehaviour, IThrowable
+public class Grenade2 : MonoBehaviour
 {
     [SerializeField] GameObject explosion;
     [SerializeField] int timer;
@@ -13,22 +13,6 @@ public class Grenade2 : MonoBehaviour, IThrowable
         yield return new WaitForSeconds(timer);
         Instantiate(explosion, transform.position, explosion.transform.rotation);
         Destroy(gameObject);
-    }
-    public void OnTriggerEnter(Collider other)
-    {
-        if(other.CompareTag("MainCamera"))
-        {
-           OnCollect();
-        }
-    }
-
-    public void OnCollect()
-    {
-       if(Input.GetKeyDown(KeyCode.Mouse0))
-       {
-            rb.useGravity = false;
-            gameObject.transform.parent = Parent;
-       }
     }
 
     // Update is called once per frame
