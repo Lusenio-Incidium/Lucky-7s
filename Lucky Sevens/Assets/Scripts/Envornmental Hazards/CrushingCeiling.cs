e8fee5bc726497246e38ad2ef9efa498eb6015cf
Najee Jackson <NJackson1@student.fullsail.edu>
2023-06-14 00:27:14 -0400

    BYE BYE VISUAL SCRIPTING

--- Lucky Sevens/Assets/Scripts/Envornmental Hazards/CrushingCeiling.cs
+++ Lucky Sevens/Assets/Scripts/Envornmental Hazards/CrushingCeiling.cs
@@ -1,7 +1,6 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class CrushingCeiling : MonoBehaviour, IButtonTrigger
