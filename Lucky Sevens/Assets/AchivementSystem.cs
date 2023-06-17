6a3df0cd99fb803001fb994f8c4e102471c39b3b
Shadow227 <118745349+Shadow227@users.noreply.github.com>
2023-06-16 22:30:17 -0400

    Added "beat X level" Achievements to all levels

--- Lucky Sevens/Assets/AchivementSystem.cs
+++ Lucky Sevens/Assets/AchivementSystem.cs
@@ -2,25 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Challanges 
+{
+    PistolOnly,
+    NoHit,
+    NoDeath
+}
 public class AchivementSystem : MonoBehaviour
 {
-    [SerializeField] int trophyID;
-
+    [SerializeField] int normalID;
+    [SerializeField] int hardID;
+    [SerializeField] int challangeID1;
+    [SerializeField] int challangeID2;
+    [SerializeField] int challangeID3;
+    [SerializeField] bool compleateLevel;
+    [SerializeField] bool hasChalange;
+    [SerializeField] bool hardMode;
+    [SerializeField] Challanges chalange1;
+    [SerializeField] Challanges chalange2;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) 
         {
-            GameJolt.API.Trophies.TryUnlock(trophyID, (GameJolt.API.TryUnlockResult result) => {
-                switch (result)
-                {
-                    case GameJolt.API.TryUnlockResult.Unlocked:
-                        break;
-                    case GameJolt.API.TryUnlockResult.AlreadyUnlocked:
-                        break;
-                    case GameJolt.API.TryUnlockResult.Failure:
-                        break;
-                }
-            });
+            if(GameJolt.API.GameJoltAPI.Instance.CurrentUser != null) 
+            {
+                if (compleateLevel)
+                    giveAchivement(normalID);
+                if (hardMode && compleateLevel && GameManager.instance.hardMode)
+                    giveAchivement(hardID);
+
+            }
         }
+    }
+
+    void giveAchivement(int id) 
+    {
+        GameJolt.API.Trophies.TryUnlock(id, (GameJolt.API.TryUnlockResult result) => {
+            switch (result)
+            {
+                case GameJolt.API.TryUnlockResult.Unlocked:
+                    break;
+                case GameJolt.API.TryUnlockResult.AlreadyUnlocked:
+                    break;
+                case GameJolt.API.TryUnlockResult.Failure:
+                    break;
+            }
+        });
     }
 }
